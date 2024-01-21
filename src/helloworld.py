6d07142fe0f3e437817bd36b3e6c79fde73386f3
abilakew <abilakew@gmail.com>
2024-01-20 21:21:55 -0500

    add helloworld.py

--- src/helloworld.py
+++ src/helloworld.py
@@ -1,18 +1,4 @@
-def hello_world(city):
-    print(f'Hello World from {city}')
-
-
 def main():
-    hello_world("NYC")
+    print('hello world')
 
 main()
-
-
-word = {'w','r','f','g','h'}
-
-def enumerat():
-    for x in word:
-        print(x)
-
-enumerat()
-
